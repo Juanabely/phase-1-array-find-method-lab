c2092a309c1a28c0f90d1720d4265912b4ee0135
Juanabely <abelkipkorir185@gmail.com>
2024-01-10 10:45:40 +0300

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// code your solution here
+superbowlWin = (record) => {
+    const result = record.find(record => record.result === "W" );
+    if(result){
+        return result.year;
+    }
+    return undefined;}
